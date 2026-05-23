8450313936e18bdf0b6f64c857aee9c7abf3a447
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-23 15:03:51 -0400

    fix: correct date typo to data in jwt.verify and remove debug console logs

--- utils/auth.js
+++ utils/auth.js
@@ -12,7 +12,7 @@ module.exports = {
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 
-  // Authenticaet and decode JWT from request
+  // Authenticate and decode JWT from request
   authMiddleware: function (req, res, next) {
     // Allow token to be sent via body, query, or headers
     let token =
@@ -33,7 +33,7 @@ module.exports = {
 
     try {
       //Verify token and attach decoded user data to request
-      const { date } = jwt.verify(token, secret, { maxAge: expiration });
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log("Invalid token");
